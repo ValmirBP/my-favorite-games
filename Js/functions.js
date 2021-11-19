dda7c374dbdde0b2c7adc5ced15e3114f5cf5902
Valmir de Barros Pedro <valmir.barros57@yahoo.com.br>
2021-11-18 21:21:50 -0300

    Funções em JS para aterar alternancia de páginas functions.js| conversão de imagem para jpg | ajuste de css base | criação de menu pagina actions | ajuste de página index

--- Js/functions.js
+++ Js/functions.js
@@ -1,5 +1,21 @@
-function GoAction(){
+function goAction(){
     location.href = './Action.html'
+};
+
+function goArcade(){
+    location.href = './Arcade.html'
+};
+
+function goFPS(){
+    location.href = './FPS.html'
+};
+
+function goFreeWorld(){
+    location.href = './FreeWorld.html'
+};
+
+function goRace(){
+    location.href = './Race.html'
 };
 
 function showActualYear() {
